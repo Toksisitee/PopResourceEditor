eac1b6be85960841beee74a9bea03891f746c371
Tox <loading.buzzz@gmail.com>
2025-02-16 08:21:00 +0200

    CEditorApp: Set/Get Populous/Editor directory paths

--- App.h
+++ App.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #include "D3DApp.h"
 
 class CEditorApp : public CD3DApp
@@ -9,11 +10,19 @@ public:
 
 	void			Run() override;
 	void			InitializeImGui();
+
+	bool			IsPopDirectorySet() const { return !m_sPopDirectory.empty(); }
+	void			SetPopDirectory( const std::string& sDir ) { m_sPopDirectory = sDir; }
+	void			SetExportDirectory( const std::string& sDir ) { m_sExportDirectory = sDir; }
+
+	const std::string& GetPopDirectory() const { return m_sPopDirectory; }
+	const std::string& GetExportDirectory() const { return m_sExportDirectory; }
+
 protected:
 	LRESULT			WndProc( UINT uMsg, WPARAM wParam, LPARAM lParam ) override;
-
 private:
-
+	std::string		m_sPopDirectory;
+	std::string		m_sExportDirectory;
 };
 
 extern CEditorApp g_Editor;
