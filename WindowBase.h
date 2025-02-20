4bb761af555e9d43f868c6e94d0fb78286b8c33a
Tox <loading.buzzz@gmail.com>
2025-02-20 09:36:04 +0200

    CWindowBase: add std::function<void()> m_PendingTask

--- WindowBase.h
+++ WindowBase.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <functional>
 
 typedef struct IDirect3DDevice9* LPDIRECT3DDEVICE9, * PDIRECT3DDEVICE9;
 
@@ -15,6 +16,13 @@ public:
 	virtual void Render() = 0;
 	virtual void Initialize( LPDIRECT3DDEVICE9 pd3dDevice ) = 0;
 	virtual void Cleanup() = 0;
+	void ProcessTask()
+	{
+		if ( m_PendingTask ) {
+			m_PendingTask();
+			m_PendingTask = nullptr;
+		}
+	}
 
 	void SetWindowName( std::string sWindowName ) { m_sWindowName = sWindowName; }
 	[[nodiscard]] virtual const std::string& GetWindowName() const { return m_sWindowName; }
@@ -25,4 +33,5 @@ protected:
 	bool                m_bOpen = true;
 	std::string         m_sWindowName;
 	LPDIRECT3DDEVICE9   m_pd3dDevice;
+	std::function<void()> m_PendingTask;
 };
