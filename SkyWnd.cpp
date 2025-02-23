07bd882e943832b2e958dfb3a40bf6f30b7e8897
Tox <loading.buzzz@gmail.com>
2025-02-23 21:58:16 +0200

    SkyWnd: reorder palette preview

--- SkyWnd.cpp
+++ SkyWnd.cpp
@@ -45,6 +45,42 @@ void CSkyWnd::Render()
 		g_ErrHandler.HandleResult( m_Sky.ExportBin( Util::FileSystem::FormatPathExportDirectory( GetWindowName() ) ) );
 	}
 
+	// Palette
+	{
+		uint8_t uIndex = 0;
+		char szColorLabel[8];
+		const int k_iColorEditFlags = ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoBorder;
+		Color* pPalette = m_Sky.GetPalette()->GetColorTable();
+
+		auto getColor = [&pPalette]( uint8_t uIndex ) -> float* {
+			float fColors[4];
+			ImColor col = ImColor( pPalette[uIndex].r, pPalette[uIndex].g, pPalette[uIndex].b );
+			memcpy( &fColors, &col, sizeof( float ) * 4 );
+			return fColors;
+		};
+
+		ImGui::PushStyleVar( ImGuiStyleVar_FramePadding, ImVec2( 5.0f, 5.0f ) );
+		ImGui::PushStyleVar( ImGuiStyleVar_ItemSpacing, ImVec2( 0.0f, 0.0f ) );
+		for ( uint32_t uIndex = Assets::Sky::k_uColorStart;
+			  uIndex < (Assets::Sky::k_uColorStart + Assets::Sky::k_uNumColors);
+			  uIndex++ ) {
+			if ( uIndex % 16 != 0 ) {
+				ImGui::SameLine();
+			}
+
+			char szColorLabel[16];
+			sprintf_s( szColorLabel, sizeof( szColorLabel ), "##%i", uIndex );
+			ImGui::ColorEdit3( szColorLabel, getColor( uIndex ), k_iColorEditFlags );
+
+			if ( ImGui::IsItemHovered() ) {
+				ImGui::BeginTooltip();
+				ImGui::Text( "Index: %d", uIndex );
+				ImGui::EndTooltip();
+			}
+		}
+		ImGui::PopStyleVar( 2 );
+	}
+
 	if ( m_Sky.GetTexture() == nullptr ) {
 		m_Sky.CreateTexture( m_pd3dDevice );
 	}
@@ -54,37 +90,4 @@ void CSkyWnd::Render()
 		ImEditor::RenderTexture( m_Sky.GetTexture() );
 		ImEditor::ResetRenderState();
 	}
-
-	uint8_t uIndex = 0;
-	char szColorLabel[8];
-	const int k_iColorEditFlags = ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoBorder;
-	Color* pPalette = m_Sky.GetPalette()->GetColorTable();
-
-	auto getColor = [&pPalette]( uint8_t uIndex ) -> float* {
-		float fColors[4];
-		ImColor col = ImColor( pPalette[uIndex].r, pPalette[uIndex].g, pPalette[uIndex].b );
-		memcpy( &fColors, &col, sizeof( float ) * 4 );
-		return fColors;
-	};
-
-	ImGui::PushStyleVar( ImGuiStyleVar_FramePadding, ImVec2( 5.0f, 5.0f ) );
-	ImGui::PushStyleVar( ImGuiStyleVar_ItemSpacing, ImVec2( 0.0f, 0.0f ) );
-	for ( uint32_t uIndex = Assets::Sky::k_uColorStart; 
-		  uIndex < (Assets::Sky::k_uColorStart + Assets::Sky::k_uNumColors); 
-		  uIndex++ ) {
-		if ( uIndex % 16 != 0 ) {
-			ImGui::SameLine();
-		}
-
-		char szColorLabel[16];
-		sprintf_s( szColorLabel, sizeof( szColorLabel ), "##%i", uIndex );
-		ImGui::ColorEdit3( szColorLabel, getColor( uIndex ), k_iColorEditFlags );
-
-		if ( ImGui::IsItemHovered() ) {
-			ImGui::BeginTooltip();
-			ImGui::Text( "Index: %d", uIndex );
-			ImGui::EndTooltip();
-		}
-	}
-	ImGui::PopStyleVar( 2 );
 }
